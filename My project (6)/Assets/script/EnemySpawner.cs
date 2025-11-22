f0664ee099edda86fb80743169398a5a47293c16
Yasi-git-hub <jasmin.modarresi@gmail.com>
2025-11-23 01:56:47 +0330

    player damage counter
    count triggers and destroy player after death

---
+++ My project (6)/Assets/script/EnemySpawner.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+
+    // Time interval between spawns.
+    public float spawnInterval = 4.0f;
+
+    // Timer to track when the next spawn should happen.
+    private float nextSpawnTime;
+
+    public int minSpawnInterval = -4;
+    public int maxSpawnInterval = 4;
+
+    private int randomPosition;
+    public bool isGameOver = false;
+
+    void Start()
+    {
+        nextSpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver) return;
+        if (Time.time >= nextSpawnTime)
+        {
+            
+            // Call the function to create the object.
+            SpawnEnemy();
+
+            // Set the time for the next spawn.
+            nextSpawnTime = Time.time + spawnInterval;
+
+             randomPosition = Random.Range(minSpawnInterval, maxSpawnInterval);
+
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 spawnPosition = new Vector3(transform.position.x + randomPosition,transform.position.y,transform.position.z);
+        Instantiate(EnemyPrefab, spawnPosition, Quaternion.identity);
+
+    }
+}
